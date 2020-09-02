281fee5eea623c893ae8826c52b64fa555164e34
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-09-02 08:53:35 -0400

    tried adding gem to help with mail confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,7 @@ Rails.application.configure do
 
   #for Devise configurations
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+config.action_mailer.raise_delivery_errors = false
 end
